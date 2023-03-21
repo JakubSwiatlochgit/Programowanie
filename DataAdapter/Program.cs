48b53def5bfc4fad2e379fbfd8bf91a9eb594e6b
Jakub Światłoch <87209356+JakubSwiatlochgit@users.noreply.github.com>
2023-03-21 16:48:55 +0100

    DataAdapter - SELECT
    Wyswietlenie za pomoca SELECT

---
+++ DataAdapter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using System.Data;
+using System.Data.SqlClient;
+namespace DataAdapter {
+
+    public class Program {
+        static public void Main() {
+            SqlConnection nwindConn = new SqlConnection("Data Source=(localdb)\\LocalServer;" +
+                "Initial Catalog=Northwind;"
+                + "Integrated Security=true");
+            SqlCommand selectCMD = new SqlCommand("SELECT * FROM Categories", nwindConn);
+            selectCMD.CommandTimeout = 30;
+            SqlDataAdapter customerDA = new SqlDataAdapter();
+            customerDA.SelectCommand = selectCMD;
+            nwindConn.Open();
+            DataSet customerDS = new DataSet();
+            customerDA.Fill(customerDS, "Categories");
+            nwindConn.Close();
+
+            Console.WriteLine("Tabela Categories:");
+            foreach (DataRow row in customerDS.Tables["Categories"].Rows)
+            {
+                Console.WriteLine($"{row["CategoryID"]} {row["CategoryName"]} {row["Description"]}");
+            }
+
+        }
+    }
+}
